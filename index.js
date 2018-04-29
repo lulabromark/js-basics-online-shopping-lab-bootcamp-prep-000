c87eb7bd0b7b0ad32dd72e54a09c7c234a1f6e8f
Mark Lawrence <lawrence.ml@gmail.com>
2018-04-29 04:45:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,5 +58,8 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (! cardNumber) {
     return("Sorry, we don't have a credit card on file for you.")
+  } else {
+    cart.length = 0;
+    
   }
 }
