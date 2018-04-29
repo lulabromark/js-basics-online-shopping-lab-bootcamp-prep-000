0277222ae596e7d00ffce7e702eeda80a96da2f8
Mark Lawrence <lawrence.ml@gmail.com>
2018-04-29 04:27:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,8 +50,9 @@ function removeFromCart(item) {
       cart.splice(i, 1);
       return(cart);
     }
-    return('That item is not in your cart.');
+    
   }
+  return('That item is not in your cart.');
 }
 
 function placeOrder(cardNumber) {
