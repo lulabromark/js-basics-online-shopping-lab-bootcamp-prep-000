34f659292058f406a902da33db995f6b31b47622
Mark Lawrence <lawrence.ml@gmail.com>
2018-04-29 04:03:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function removeFromCart(item) {
     if (item === cart[i].itemName) {
       cart.splice(i, 1);
       return(cart);
-    } else if (item {
+    } else if (item !== cart[i].itemName) {
       return('That item is not in your cart.');
     }
   }
